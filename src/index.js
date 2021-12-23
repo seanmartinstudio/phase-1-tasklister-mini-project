43e15baf0e6e5dca749ef57f3842c2df143d05e7
Sean Martin <sean@seanmartinstudio.com>
2021-12-22 17:17:08 -0800

    Lab finished

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector('form').addEventListener('submit', (e) => {
-    e.preventDefault()
-    console.log(e.target.new-task-description)
-  })
+//We want to grab the form and all of the relevant input fields
+const form = document.getElementById('create-task-form')
+
+//Attached event listener now
+form.addEventListener('submit', createTask)
 })
 
+function createTask(event) {
+//Stop the form from trying to submit
+event.preventDefault()
+const input = document.getElementById('new-task-description')
+const li = document.createElement('li')
+li.innerText = input.value
+//grab UL and append LI's to it
+const ul = document.getElementById('tasks')
+ul.appendChild(li)
+}
